09cd9e24eb8e2257e39fcca154d47c0978b89f4f
wm4 <wm4@nowhere>
2016-08-31 19:05:45 +0200

    m_config: introduce basic mechanism to synchronize global option updates
    The way option runtime changes are handled is pretty bad in the current
    codebase. There's a big option struct (MPOpts), which contains almost
    everything, and for which no synchronization mechanism exists. This was
    handled by either making some options read-only after initialization,
    duplicating the option struct, using sub-options (in the VO), and so on.
    
    Introduce a mechanism that creates a copy of the global options (or
    parts of it), and provides a well-defined way to update them in a
    thread-safe way.
    
    Most code can remain the same, just that all the component glue code has
    to explicitly make use of it first.

--- common/global.h
+++ common/global.h
@@ -5,9 +5,14 @@
 // The only purpose of this is to make mpv library-safe.
 // Think hard before adding new members.
 struct mpv_global {
-    struct MPOpts *opts;
     struct mp_log *log;
     struct mp_client_api *client_api;
+
+    struct m_config_shadow *config;
+
+    // Using this is deprecated and should be avoided (missing synchronization).
+    // Use m_config_cache to access mpv_global.config instead.
+    struct MPOpts *opts;
 };
 
 #endif
